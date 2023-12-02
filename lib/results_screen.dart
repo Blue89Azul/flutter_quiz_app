40e3100435ec064f97b1ff3702bd28efb317cc44
Blue89Azul <life.89.blue@gmail.com>
2023-12-02 10:00:44 +0900

    display details of user's answers

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,26 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.resetAnswer,
+  });
+
+  final List<String> chosenAnswers;
+  final void Function() resetAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, Object>> summaryData = getSummaryData();
+    final int numTotalQuestions = questions.length;
+    final int numCorrectQuestions = summaryData.where((data) {
+      return data['correct_answer'] == data['user_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
-        margin: EdgeInsets.all(40),
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You answered X out of Y questions correctly!'),
-            SizedBox(
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: const TextStyle(color: Colors.white, fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
               height: 30,
             ),
-            Text('List of questions...'),
-            SizedBox(
+            QuestionsSummary(summaryData: summaryData),
+            const SizedBox(
               height: 30,
             ),
-            TextButton(onPressed: () {}, child: const Text('Restart Quiz!' , style: TextStyle(color: Colors.white),))
+            TextButton(
+                onPressed: () {
+                  resetAnswer();
+                },
+                child: const Text(
+                  'Restart Quiz!',
+                  style: TextStyle(color: Colors.white),
+                ))
           ],
         ),
       ),
